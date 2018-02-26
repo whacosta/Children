ca113d713015277468bb6129750dcf9d286e94c1
whacosta <whacosta@fiec.espol.edu.ec>
2018-02-25 23:54:57 -0500

    delete

--- administracion/programas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProgramasConfig(AppConfig):
-    name = 'programas'
--- administracion/usuarios/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Nino
-class NinoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Nino
-        fields='__all__'
